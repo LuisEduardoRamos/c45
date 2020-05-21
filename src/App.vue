b5ffc528fefdf45e9b8c084f48cf682697f639ea
LuisEduardoRamos <luiseduardoramosg@gmail.com>
2020-05-20 18:44:04 -0700

    Primer commit, configuraciones y algo mas

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container fluid>
+        <b-row>
+          <b-col>
+            <b-navbar toggleable type="dark" variant="dark">
+              <b-navbar-brand href="https://github.com/LuisEduardoRamos">Luis Eduardo Ramos Granados 15171256</b-navbar-brand>
+            </b-navbar>            
+          </b-col>
+        </b-row>
+        <b-row>
+          
+        </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
