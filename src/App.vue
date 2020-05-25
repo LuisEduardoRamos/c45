600a80817b9530ad2238d44e97a2f65e39e47efe
LuisEduardoRamos <luiseduardoramosg@gmail.com>
2020-05-25 00:52:53 -0700

    Para deploy

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,11 @@
             </b-navbar>            
           </b-col>
         </b-row>
+        <b-row style="margin-top:10px;">
+          <b-col>
+            <b-alert show variant="danger">No igresar datasets con columna de indexado.</b-alert>
+          </b-col>
+        </b-row>
         <b-row id="rowInput" cols="8">
           <b-col>
             <b-form-file
@@ -19,15 +24,40 @@
             ></b-form-file>
           </b-col>
           <b-col cols="2">
-            <b-button @click="parsearCsv" variant="outline-primary">Calcular</b-button>
+            <b-button @click="parsearCsv" variant="outline-primary">Parsear CSV</b-button>
           </b-col>
         </b-row>
         <b-row>
           <b-col cols="3">
             <div class="mt-3 black">Archivo seleccionado: </div>
-          </b-col>
-          <b-col>
             <div class="mt-3">{{ file ? file.name : '' }}</div>
+          </b-col>
+          <b-col cols="3">
+            <div class="mt-3">
+              <div class="mt-3 black">Clase de decisión: </div>
+              <b-form-select v-model="decision" :options="options"></b-form-select>
+            </div>
+          </b-col>
+          <b-col cols="3">
+            <div class="mt-3">
+              <div class="mt-3 transparent">h</div>
+              <b-button @click="c45" variant="success">Calcular C4.5</b-button>
+            </div>
+          </b-col>
+        </b-row>
+        <b-row style="margin-top:10px;" :key="item.iteracion" v-for="item in response">
+          <b-col>
+            <b-list-group >
+              <b-list-group-item variant="dark">Iteración: {{item.iteracion}}</b-list-group-item>
+              <b-list-group-item>Relación: {{item.relacion || '-'}}</b-list-group-item>
+              <b-list-group-item>Clase ganadora: {{item.claseGanadora|| '-'}}</b-list-group-item>
+              <b-list-group-item>Entropía global: {{item.entropiaGlobal }}</b-list-group-item>
+              <b-list-group-item class="black">Hijos clase ganadora:</b-list-group-item>
+              <b-list-group-item variant="success" v-for="x in item.hijosGanadora">{{x}}</b-list-group-item>
+              <b-list-group-item class="black">Entropías:</b-list-group-item>
+              <b-list-group-item variant="success" v-for="y in item.entropias">{{`Entripía de la clase ${y.clase}: ${y.entropia} `}}</b-list-group-item>
+              <b-list-group-item variant="info">Desicion final: {{item.desicionFinal|| '-'}}</b-list-group-item>
+            </b-list-group>
           </b-col>
         </b-row>
     </b-container>
@@ -37,29 +67,38 @@
 <script>
 
 import axios from 'axios';
+import c45 from './util/c45';
 
 export default {
   name: 'App',
   data() {
     return {
-      file:null
+      file:null,
+      responseCsv:null,
+      decision: null,
+      options:[],
+      responseC45:[],
+      response:[]
     }
   },
   methods:{
-    parsearCsv(){
-      let formData = new FormData();
-      formData.append('file', this.file);
-      axios.post('192.168.2.10:3000/api/upload', 
-      formData, formData, {
+    async parsearCsv(){
+      const formData = new FormData();
+		  formData.append('csv', this.file);
+      this.responseCsv = await axios.post('http://192.168.2.10:3000/api/upload', formData , {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
-      }).then(function(response){
-        console.log(respose);
-      }).catch(function(err){
-        console.log(err);
-      })
-    }
+      });
+      console.log(this.responseCsv.data.clases)
+      this.options = this.responseCsv.data.clases.map((clase)=>({ value:clase,text:clase }));
+    },
+    async c45(){
+      this.response = [];
+      this.response = await c45(this.responseCsv.data.datos, this.decision);
+      console.log(this.response)
+    },
+
   }
 }
 </script>
@@ -67,6 +106,9 @@ export default {
 <style>
   .black{
     font-weight: bold;
+  }
+  .transparent{
+    color:white;
   }
   #rowInput{
     margin-top: .5em;
---
+++ src/util/c45.js
@@ -0,0 +1,107 @@
+const _ = require('lodash');
+let iteracion = 0;
+let arbolArray = [];
+let iteracionArray = [];
+let iteracionObject = new Object();
+let entropiasArray = [];
+let relacionTemporal = null;
+
+function c45(dataset, claseDecision){
+    iteracionObject = {};        
+    iteracion+=1;
+    let entropiaMenor = 1;
+    let entropiaGo = calcularEntropia(dataset, claseDecision);
+    let claseSeleccionada = '';
+    let data = agruparDatos(dataset);
+    let total = dataset.length;
+    let entropias = [];
+    const clases = Object.keys(dataset[0]);
+    iteracionObject.iteracion = iteracion;
+    iteracionObject.entropiaGlobal = entropiaGo;
+    if(relacionTemporal){
+        iteracionObject.relacion = relacionTemporal;
+        relacionTemporal = null;
+    }
+    for (let i = 0; i < data.length; i++) {
+        const nombreClase = clases[i];
+        console.log(nombreClase)
+        if (nombreClase === claseDecision)break;
+        const clase = data[i];
+        const dataClase  = clase[nombreClase];
+        const opciones = Object.keys(dataClase);
+        let entropiaParcial = 0;
+        //Sumarización
+        for (let j = 0; j < opciones.length; j++) {
+            let sub = dataClase[opciones[j]];
+            let entropySub = calcularEntropia(sub, claseDecision);
+            let x = (sub.length/total) * entropySub;
+            entropiaParcial += x;
+            if(entropiaGo == 0) {
+                let hoja = sub[0];
+                iteracionObject.desicionFinal = hoja[claseDecision];
+                return;
+            }
+        }
+        entropiasArray.push({clase: nombreClase, entropia: entropiaParcial});
+        if(entropiaParcial < entropiaMenor){
+            entropiaMenor = entropiaParcial;
+            claseSeleccionada = nombreClase;
+        }
+        let entropiaObject = {[nombreClase]:entropiaParcial};
+        entropias.push(entropiaObject);
+    }
+    const index = clases.indexOf(claseSeleccionada);
+    const dataSelecArray = data.splice(index, 1);
+    const dataSelecObj = dataSelecArray[0];
+    const dataSelecKeys = Object.keys(dataSelecObj[claseSeleccionada]);
+    const dataSelec = dataSelecObj[claseSeleccionada];
+    iteracionObject.hijosGanadora = dataSelecKeys;
+    iteracionObject.entropias = entropiasArray;
+    entropiasArray = [];
+    for (let i = 0; i < dataSelecKeys.length; i++) {
+        let datosNuevos = dataSelec[dataSelecKeys[i]]
+        relacionTemporal = dataSelecKeys[i] + ' -(hijo)-> ' + claseSeleccionada;
+        iteracionObject.claseGanadora = claseSeleccionada;
+        iteracionArray.push(iteracionObject);
+        c45(datosNuevos, claseDecision);
+    }
+
+}
+
+function calcularEntropia(datos, clase){
+    let total = datos.length;
+    let tipos = _.uniqBy(datos, clase);
+    let grupos = _.groupBy(datos, clase);
+    let tiposArray = [];
+    let entropiaClase = 0;
+    //Extrae los datos de la clase, sin repetir
+    for (let i = 0; i < tipos.length; i++) {
+        let json =  tipos[i];
+        tiposArray.push(json[clase]);
+    }
+    //Se calcula la entropía iterando las opciones
+    for (let i = 0; i < tiposArray.length; i++) {
+        //grupos es un arreglo donde se encuentran todos los datos agrupados por las opciones de la clase
+        let cantidadTipos = grupos[tiposArray[i]].length;
+        let relacion = cantidadTipos / total;
+        entropiaClase += entropia(relacion);
+    }
+    return entropiaClase;
+}
+
+function entropia(relacion){
+    return ((relacion) * Math.log2(relacion) * -1);
+}
+
+function agruparDatos(datos){
+    //Se extren las cabeceras del csv, en este caso pasan a ser las llaves del objeto
+    let cabeceras = Object.keys(datos[0]);
+    return cabeceras.map((cabecera)=>({
+       [cabecera]: _.groupBy(datos, cabecera)
+    }));
+}
+
+module.exports = (json, desicion) => {
+    c45(json, desicion)
+    return iteracionArray;
+}
